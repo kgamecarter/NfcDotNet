51c385dbb8856f26bfc845ea61a81cfecb66f533
kgame <kgamecarter@gmail.com>
2018-10-07 15:05:08 +0800

    回傳table

--- NfcDotNet/Program.cs
+++ NfcDotNet/Program.cs
@@ -30,7 +30,7 @@ namespace NfcDotNet
         static byte[] abtRx = new byte[MAX_FRAME_LEN];
 
         static Random rnd = new Random();
-        static bool filterFunctionEx()
+        static int filterFunctionEx()
         {
             var s = new byte[]
             {
@@ -51,25 +51,6 @@ namespace NfcDotNet
                 0x2A,
                 0xAA
             };
-            var s2 = new byte[]
-            {
-                0x00,
-                0x02,
-                0x08,
-                0x0A,
-                0x20,
-                0x22,
-                0x28,
-                0x2A,
-                0x80,
-                0x82,
-                0x88,
-                0x8A,
-                0xA0,
-                0xA2,
-                0xA8,
-                0xAA,
-            };
             ulong key = 0;
             for (int i = 0; i < 5; i++)
                 key = key << 8 | (byte)rnd.Next(0xFF);
@@ -78,20 +59,19 @@ namespace NfcDotNet
             for (int i = s.Length - 1; i >= 0; i--)
                 Console.Write("{0:x2} ", s[i]);
             Console.WriteLine();
-            int sum = 0;
             int fb = 0;
             for (int i = s.Length - 1; i >= 0; i--)
             {
                 using (var c = new Crapto1(key | (ulong)s[i]))
                 {
-                    fb = fb << 1| c.PeekCrypto1Bit();
-                    Console.Write(" {0} ", c.PeekCrypto1Bit());
-                    sum += c.PeekCrypto1Bit();
+                    var ks = c.PeekCrypto1Bit();
+                    fb = fb << 1 | ks;
+                    Console.Write(" {0} ", ks);
                 }
             }
             Console.WriteLine();
             Console.WriteLine("0x{0:x4}", fb);
-            return sum != 0 && sum != 16;
+            return fb;
         }
 
         static void Main(string[] args)
